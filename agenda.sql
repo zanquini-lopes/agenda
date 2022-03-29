525c1dcf6b7cb05d2dc387da646d1c25f5fe2abc
Rodrigo Zanquini Lopes <rodrigo.zanquini.lopes@gmail.com>
2022-03-28 23:53:07 -0300

    Criação das Classe Controller e Index.html

---
+++ agenda.sql
@@ -0,0 +1,10 @@
+create database dbagenda;
+show databases;
+use dbagenda;
+create table contatos(
+	idcon int primary key auto_increment,
+    nome varchar(50) not null,
+    fone varchar(15) not null,
+    email varchar(50)
+    );
+describe contatos;
