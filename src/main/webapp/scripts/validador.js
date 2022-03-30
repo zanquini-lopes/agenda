1fcdb38a870f47aac9544ec2f5d0c0ca41169b34
Rodrigo Zanquini Lopes <rodrigo.zanquini.lopes@gmail.com>
2022-03-30 00:21:15 -0300

    Implantando o CRUD - CREATE

---
+++ src/main/webapp/scripts/validador.js
@@ -0,0 +1,22 @@
+/**
+ * Validação de formulário
+ * @author Rodrigo Zanquini Lopes
+ */
+ 
+ function validar() {
+	let nome = frmContato.nome.value
+	let fone = frmContato.fone.value
+	if (nome === "") {
+		alert('Preencha o campo nome')
+		frmContato.nome.focus()
+		return false
+	} else if (fone === "") {
+		alert('Preencha o campo de Telefone')
+		frmContato.fone.focus()
+		return false
+	} else {
+		document.forms["frmContato"].submit()
+	}
+}
+
+ 
