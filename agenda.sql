0cb2b9c8515a83186f341c29a720627bada7a275
Rodrigo Zanquini Lopes <rodrigo.zanquini.lopes@gmail.com>
2022-04-06 23:47:26 -0300

    CRUD Delete - Confirmação de exclusão - Parte 1/2

--- agenda.sql
+++ agenda.sql
@@ -13,14 +13,17 @@ describe contatos;
  insert into contatos (nome, fone, email) values ('Bill Gates', '99999-9999', 'bill@outlook.com');
  
  
- 
- /*CRUD READ*/
+  /*CRUD READ*/
  select * from contatos;
  select * from contatos order by nome;
  select * from contatos where idcon=1;
  
  /*CRUD UPDATE*/
  update contatos set nome='Bruce' where idcon =1;
+ 
+ /*CRUD DELETE*/
+  select * from contatos where idcon=1;  
+ 
  
  
  
